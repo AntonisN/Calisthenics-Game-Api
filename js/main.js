f70ca29ebc9f52e6eaa1ab8fe4f49327161e9cdc
AntonisN <antwnhsftw@gmail.com>
2021-06-24 08:39:37 +0300

    added Procfile

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// document.querySelector('button').addEventListener('click', getCaliExercise)
+
+// async function getCaliExercise() {
+//   const nameOfEx = document.querySelector('input').value
+//     try {
+//       const res = await fetch
+//     } catch (err) {
+//         console.log(err)
+//     }
+// }
